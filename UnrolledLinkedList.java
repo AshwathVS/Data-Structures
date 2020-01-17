fb99bcabe63ad968b81b6301221d9457c51cc529
AshwathVS <ashsat.vijayan122@gmail.com>
2020-01-17 11:03:51 +0000

    base api description

---
+++ UnrolledLinkedList.java
@@ -0,0 +1,78 @@
+class UnrolledLinkedList<T> {
+
+    private int nodeCount;
+
+    private int numberOfElements;
+
+    private int numberOfElementsPerNode;
+
+    private Node<T> front;
+
+    private Node<T> back;
+
+    public UnrolledLinkedList(int numberOfElementsPerNode) {
+        this.numberOfElementsPerNode = numberOfElementsPerNode;
+    }
+
+    public UnrolledLinkedList() {
+        this(8);
+    }
+
+    public void remove(int index) {
+        
+    }
+
+    public void remove(T data) {
+
+    }
+
+    public void add(T data) {
+
+    }
+
+    public void add(T data, int index) {
+
+    }
+
+    public int size() {
+        return this.numberOfElements;
+    }
+
+    public T find(T data) {
+
+    }
+
+    public static class Node<T> {
+        private int countOfElements;
+        private T[] arr;
+        private Node<T> next;
+
+        public int getCountOfElements() {
+            return countOfElements;
+        }
+
+        public void setCountOfElements(int countOfElements) {
+            this.countOfElements = countOfElements;
+        }
+
+        public T[] getArr() {
+            return arr;
+        }
+
+        public void setArr(T[] arr) {
+            this.arr = arr;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello..");
+    }
+}
